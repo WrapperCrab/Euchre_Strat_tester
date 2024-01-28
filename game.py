3a6cad354bc93df5b3a6b8094a65e56c2060454a
Cross, Winter Isaac <wi928rsnowflake@gmail.com>
2024-01-28 14:43:30 -0500

    Update game.py

--- game.py
+++ game.py
@@ -82,12 +82,177 @@ class Game:
                 playersInTrick.append(p)
                 self._hands[p].remove(card)
             winningCard = self.best_card(trick,self.trump,ledSuit)
+            winningPlayer = playersInTrick[trick.index(winningCard)]
+            self.tricksScore[self.team_num_for(winningPlayer)]+=1
 
+            self._rotate_until_first(winningPlayer)
+            if printOutput:
+                print(winningPlayer.name,winningCard,trick)
+        #score
+        self.score_hand()
+
+        #reset
+        self.trump = None
+        self.top_card = None
+        self.inactives = []
+        self.dealer = None
+        self.caller = None
+        self.tricksScore[1] = 0
+        self.tricksScore[2] = 0
+        for p in self.playersOrder:
+            self.hands[p] = []
+            p.active = True
+    def deal_hand(self):
+        self._deck = [value + suit for value in VALUES for suit in SUITS]
+        shuffle(self._deck)
+        parityDeal = 0
+        for index in range(4):
+            p=self.playersOrder[index]
+            cardsToDeal = 0
+            if parityDeal%2==0:
+                cardsToDeal = 3
+            else:
+                cardsToDeal = 2
+            parityDeal+=1
+            for _ in range(cardsToDeal):
+                card = self._deck.pop()
+                self._hands[p].append(card)
+        for index in range(4):
+            p = self.playersOrder[index]
+            for _ in range(5 - len(self._hands[p])):
+                card = self._deck.pop()
+                self._hands[p].append(card)
+        self.topCard = self._deck.pop()
+    def call_trump(self,printOutput):
+        if printOutput:
+            print("top card", self.topCard)
+        for index in range(4):
+            p=self.playersOrder[index]
+            callResult = p.call(self.topCard)#True, False, or "alone"
+            if callResult == False:
+                continue
+            #the suit has been called
+            self.trump = self.topCard[1]
+            self._hands[self.dealer].append(self.topCard)
+            discard = self.dealer.discard()
+            self._hands[self.dealer].remove(discard)
+            goingAlone = False
+            if callResult == "alone":
+                #make the partner inactive
+                goingAlone = True
+                self.inactives.append(self.teammate_for(p))
+                self.teammate_for(p).active = False
+                if self.playersOrder[0]==self.teammate_for(p):
+                    #The inactive player is first, rotate
+                    #This implements left of the dealer rules
+                    self._rotate()
+                if printOutput:
+                    print(p.name, ":",self.trump," Alone!")
+            else:
+                if printOutput:
+                    print(p.name,":",self.trump)
+            self.caller = p
+            return
+        if printOutput:
+            print("top card flipped over")
+
+        #second pass
+        for playerIndex in range(4):
+            p=self.playersOrder[playerIndex]
+            call = p.call2(self.topCard)
+            if call[1]==False:
+                #they passed
+                if p==self.dealer:
+                    raise IllegalPlayException("The dealer go screwed but didn't call a suit")
+                #this is a legal pass
+                if printOutput:
+                    print(p.name,":",self.trump)
+            else:
+                #they called
+                callResult = call[0]
+                if callResult == self.topCard[1]:
+                    raise IllegalPlayException("Can't call the face up card after it's flipped")
+                self.trump = callResult
+                self.caller = p
+                goingAlone = False
+                if call[1]=="alone":
+                    # make the partner inactive
+                    goingAlong = True
+                    self.inactives.append(self.treammate_for(p))
+                    self.teammate_for(p).active = False
+                    if self.playersOrder[0]==self.teammate_for(p):
+                        #The inactive player is first. rotate
+                        #This implements left of the dealer rules
+                        self._rotate()
+                    if printOutput:
+                        print(p.name,":",self.trump," Alone!")
+                else:
+                    #they did not go alone
+                    if printOutput:
+                        print(p.name,":",self.trump)
+                return
+
+
+    def score_hand(self):
+        callingTeamNum = self.team_num_for(self.caller)
+        nonCallingTeamNum = (callingTeamNum%2)+1
+        callingTeam = self.teams[callingTeamNum-1]
+        nonCallingTeam = self.teams[nonCallingTeamNum-1]
+        if self.tricksScore[callingTeamNum]>self.tricksScore[nonCallingTeamNum]:
+            #calling team won
+            if self.tricksScore[callingTeamNum] == 5:
+                #calling team got all 5
+                if (callingTeam[0] in self.inactives) or (callingTeam[1] in self.inactives):
+                    #calling team made a loner
+                    self.gameScore[callingTeamNum]+=4
+                else:
+                    #calling team did not go alone
+                    self.gameScore[callingTeamNum]+=2
+            else:
+                #Calling team did not get all 5
+                self.gameScore[callingTeamNum]+=1
+        else:
+            #calling team lost
+            self.gameScore[nonCallingTeamNum]+=2
+    def print_hand(self):
+        #print the hand of each player
+        print("------------------- Trump:", self._trump, "---------------")
+        for index in range(4):
+            p = self.playersOrder[index]
+            if p not in self.inactives:
+                print(self.position_for(p),p.name,self.hands[p])
+            else:
+                print(self.position_for(p),p.name,"*** asleep ***")
 
 
     def hand_for(self,player):
         #returns the hand of player
         return self._hands[player]
+    def team_num_for(self,player):
+        #return the team number of player
+        if player in self.teams[0]:
+            return 1
+        elif player in self.teams[1]:
+            return 2
+        else:
+            raise Exception("You are not in this game!!??")
+    def position_for(self,player):
+        #returns the position of player in players list with respect to dealer as position 3
+        playerPosition = 0
+        for playerIndex in range(4):
+            if self.players[playerIndex]==player:
+                playerPosition = playerIndex
+                break
+        return ((3+(playerPosition-self.dealerIndex))%4)
+    def teammate_for(self,player):
+        #return the teammate of player
+        for playerIndex in range(4):
+            if (self.playersOrder[playerIndex]==player):
+                return self.playersOrder[(playerIndex+2)%4]
+    def is_player_active(self,player):
+        if player in self.inactives:
+            return False
+        return True
 
     def get_suit(self,card,trump):
         if (card[0]=='J') and (card[1]==self.next_suit(trump)):
@@ -104,7 +269,6 @@ class Game:
                 return 's'
             case 'd':
                 return 'h'
-
     def best_card(self,cards,trump=None,led=None):
         #returns the winning card from cards given trump and led suits
         #correct even if trump and or led are None
@@ -118,12 +282,12 @@ class Game:
                     score+=40
                 else:
                     score+=20
-            elif (trump == next_suit(card[1])) and (c[0]=='J'):
+            elif (trump == self.next_suit(card[1])) and (card[0]=='J'):
                 #this is the left
                 score+=30
             else:
                 #this is not trump
-                if led==c[1]:
+                if led==card[1]:
                     #this is led suit
                     score+=10
                 else:
@@ -148,7 +312,6 @@ class Game:
                 bestCard = card
                 bestCardScore = score
         return bestCard
-
 
     def _rotate_until_dealer(self,dealerIndex):
         while self.playersOrder[3]!=self._players[dealerIndex]:
